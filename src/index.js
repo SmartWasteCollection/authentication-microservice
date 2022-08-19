c5a8532ab0de613818c0ba8d414653e4616d749d
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-08-19 15:30:07 +0200

    chore: fix linter setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-const { createServer } = require("http");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const { createServer } = require('http');
 
-require("dotenv").config();
+require('dotenv').config();
 
 global.appRoot = path.resolve(__dirname);
 
@@ -13,20 +13,20 @@ const PORT = 3002;
 
 app.use(cors());
 app.use(express.json());
-app.use("/static", express.static(__dirname + "/public"));
+app.use('/static', express.static(__dirname + '/public'));
 
 mongoose.connect(process.env.URI_MONGO_DB,
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    (e) => e == null ? console.log("Connected to mongoDB") : console.log("ERROR on connection: " + e));
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (e) => e == null ? console.log('Connected to mongoDB') : console.log('ERROR on connection: ' + e));
 
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello!");
+app.get('/', (req, res) => {
+    res.status(200).send('Hello!');
 });
 
-app.use((req, res) => res.status(404).send({url: req.originalUrl + " not found"}));
+app.use((req, res) => res.status(404).send({ url: req.originalUrl + ' not found' }));
 
 const httpServer = createServer(app);
-httpServer.listen(PORT, () => console.log("Node API server started on port "+PORT) );
+httpServer.listen(PORT, () => console.log('Node API server started on port '+PORT) );
 
 module.exports = app;
