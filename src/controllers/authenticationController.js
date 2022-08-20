9c363ffa90fb25c124bac794e1d8c64c06fee15a
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-08-20 16:50:40 +0200

    feat: add password hashing

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,22 +1,55 @@
 const User = require('../models/UserModel');
+const bcrypt = require('bcryptjs');
 
 exports.register = (req, res) => {
-    new User(req.body).save((err, user) => {
-        if (err) {
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+        req.body.password = hash;
+        new User(req.body).save((err, user) => {
+            if (err) {
+                res.status(401).json({
+                    message: 'User not successful created',
+                    error: err
+                });
+            } else {
+                res.status(200).json({
+                    message: 'User successfully created',
+                    user,
+                });
+            }
+        });
+    });
+};
+
+exports.login = (req, res) => {
+    console.log('Login request');
+    User.findOne({ username: req.body.username }, (err,user) => {
+        if (!user) {
             res.status(401).json({
-                message: 'User not successful created',
-                error: err
+                message: 'Login not successful',
+                error: 'Email or password wrong',
             });
         } else {
-            res.status(200).json({
-                message: 'User successfully created',
-                user,
+            bcrypt.compare(req.body.password, user.password).then(function(result) {
+                result
+                    ? res.status(200).json({
+                        message: 'Login successful',
+                        user,
+                    })
+                    : res.status(401).json({
+                        message: 'Login not succesful',
+                        error: 'Email or password wrong' });
+            });
+        }
+        if (err) {
+            res.status(400).json({
+                message: 'An error occurred',
+                error: err.message,
             });
         }
     });
 };
 
-exports.getAllUsers = (req, res) =>{
+exports.getAllUsers = (req, res) => {
     User.find({}, (err, users) => {
         if (!err) {
             res.json(users);
--- src/routes/authenticationRoutes.js
+++ src/routes/authenticationRoutes.js
@@ -2,6 +2,9 @@ module.exports = function(app, authenticationController) {
     app.route('/authentication/register')
         .post(authenticationController.register);
 
+    app.route('/authentication/login')
+        .post(authenticationController.login);
+
     app.route('/authentication/users')
         .get(authenticationController.getAllUsers);
 };
